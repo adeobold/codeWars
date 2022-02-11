8a73390ce5a054f87371ba6a3f244b71da393c47
adeobold <adeobold@gmail.com>
2022-02-11 13:27:54 +0300

    createPhoneNumber

--- src/main/java/codewars/Training.java
+++ src/main/java/codewars/Training.java
@@ -20,7 +20,21 @@ public class Training {
                 true, true, true, true,
                 false, false, true, true};
 
-        System.out.println(bouncingBall(2.272727, 0.66, 1.5));
+        System.out.println(createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        // Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+        //
+        //Example
+        //Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+        //The returned format must be correct in order to complete this challenge.
+        //Don't forget the space after the closing parentheses!
+
+        return  "(" + numbers[0] + numbers[1] + numbers[2] + ") " +
+                numbers[3] + numbers[4] + numbers[5] + "-" +
+                numbers[6] + numbers[7] + numbers[8] + numbers[9];
+
     }
 
     public static int bouncingBall(double h, double bounce, double window) {
