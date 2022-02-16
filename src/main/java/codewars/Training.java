708474cfb6161ea88a4c9211196f2c49d5f21d71
adeobold <adeobold@gmail.com>
2022-02-16 13:18:12 +0300

    mexican wave

--- src/main/java/codewars/Training.java
+++ src/main/java/codewars/Training.java
@@ -20,8 +20,45 @@ public class Training {
                 true, true, true, true,
                 false, false, true, true};
 
-        //System.out.println(Arrays.toString(productFib(1)));
-        System.out.println(GetSum(0, 5));
+        System.out.println(Arrays.toString(wave("two words")));
+        //System.out.println(GetSum(0, 5));
+
+    }
+
+
+    public static String[] wave(String str) {
+
+        String[] result = new String[str.replaceAll(" ", "").length()];
+
+        int arrIndex = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) != ' ') {
+
+                result[arrIndex] = str.substring(0, i) + str.substring(i, i+1).toUpperCase() + str.substring(i+1);
+
+                arrIndex++;
+
+            }
+
+        }
+
+        return result;
+
+        //Task
+        //In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+        //Rules
+        // 1.  The input string will always be lower case but maybe empty.
+        // 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+
+        //      List<String> list = new ArrayList<>();
+        //      for (int i = 0; i < str.length(); i++) {
+        //        char ch = str.charAt(i);
+        //        if (ch == ' ') continue;
+        //        list.add(str.substring(0,i) + Character.toUpperCase(ch) + str.substring(i+1));
+        //      }
+        //      return list.toArray(new String[0]);
 
     }
 
